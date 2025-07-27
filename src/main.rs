97064a1bd8b698c486ee1fd59cedb08c1c3c7228
shubham23471 <shubhamshakya61@gmail.com>
2025-07-27 15:51:37 +0530

    done logic to print src and alt values

--- src/main.rs
+++ src/main.rs
@@ -14,39 +14,46 @@ struct CountInfo {
     response_count: i32, 
     request_count: i32,
     metadata_count: i32,
+    total_src_count: i32,
 }
 
 // &Handle: as we want to pass this without taking the ownership or cloning
 #[allow(unused_variables)]
-fn walk(node: &Handle) {
+fn walk(node: &Handle, all_src: &mut Vec<String>, 
+        all_alt: &mut Vec<String>) {
+
     match &node.data {
         NodeData::Element {name, attrs, ..} => {
-            if name.local.to_string() == "img"{
-                println!("---------------------");
-                println!("NAME: {:?}", name.local.to_string());}
+            let mut src = None; 
+            let mut alt = None;
 
-                let mut src = None; // Type: Option<String> : that why we have wrap value of src in Some()
-                let mut alt = None;
+            for attr in attrs.borrow().iter() {
+                let attr_name = attr.name.local.to_string(); 
 
-                for attr in attrs.borrow().iter() {
-                    let attr_name = attr.name.local.to_string(); 
-                    if attr_name == "src" {
-                        src = Some(attr.value.to_string());
-                    } else if attr_name == "alt"{
-                        alt = Some(attr.value.to_string());
-                    }
-                // println!("Attr Name: {}, Attr Value: {}", attr.name.local, attr.value);
-                println!("src value: {:?}", src);
-                println!("alt value: {:?}", alt);
-
+                if attr_name == "src" {
+                    src = Some(attr.value.to_string());
+                } else if attr_name == "alt"{
+                    alt = Some(attr.value.to_string());
                 }
-                
+            }
+            
+            // appending the non-null values to vector
+            if let Some(src_value) = src {
+                if (src_value.starts_with("http:") || src_value.starts_with("https:")) 
+                && 
+                (src_value.ends_with("png") || src_value.ends_with("jpeg")) { 
+                    all_src.push(src_value); 
+                    if let Some(alt_value) = alt{
+                        all_alt.push(alt_value);
+                    }
+                }
+            }   
         },
         _ => {}
     }
     // recurse on children so we visit the whole tree
     for child in node.children.borrow().iter(){
-        walk(child);
+        walk(child, all_src, all_alt);
     }
 }
 
@@ -58,9 +65,10 @@ fn main() ->  Result<(), Box<dyn std::error::Error>> {
     let mut res_count = 0;
     let mut req_count = 0;
     let mut meta_count = 0;
+    let mut src_count: i32 = 0;
     
     for record in warc_file.iter_records(){
-        println!("====");
+        // println!("====");
         count += 1;
 
         // println!("{}: {}", WarcHeader::RecordID, record?.warc_id());
@@ -69,7 +77,6 @@ fn main() ->  Result<(), Box<dyn std::error::Error>> {
         match record {
             Err(e) => println!("ERROR: {}", e),
             Ok(record) => {
-                println!("{}: {}", WarcHeader::RecordID, record.warc_id());
                 // println!("{}: {}", WarcHeader::WarcType, record.date());        
                 // println!("{}: {}", WarcHeader::WarcType, record.warc_type());
                 // println!("Body: {:?}", std::str::from_utf8(record.body())?);
@@ -87,10 +94,19 @@ fn main() ->  Result<(), Box<dyn std::error::Error>> {
                     // "I'm giving you a String and I expect it to convert into the right Tendril type."
                     
                     let document: Handle = dom.document;
-                    walk(&document);
+                    let mut all_src = Vec::new();
+                    let mut all_alt = Vec::new();
+
+                    walk(&document, &mut all_src, &mut all_alt);
+
+                    src_count += all_src.len() as i32;
+                    println!("{}: {}", WarcHeader::RecordID, record.warc_id());
+                    println!("SRC value: {}", all_src.len());
+                    println!("ALT value: {}", all_alt.len());
+                    println!("SRC value: {:?}", all_src);
+                    println!("ALT value: {:?}", all_alt);
+                    println!("-------------------------");
                     
-
-
 
                 } else if record.warc_type().to_string() == "request" {
                     // println!("Body: {:?}", std::str::from_utf8((record.body()))?); 
@@ -101,8 +117,8 @@ fn main() ->  Result<(), Box<dyn std::error::Error>> {
                 }
             }
         }
-     
-        if *&count > 3 {
+
+        if *&count > 6 {
             break
         }
     }
@@ -114,8 +130,9 @@ fn main() ->  Result<(), Box<dyn std::error::Error>> {
         response_count: res_count, 
         request_count: req_count,
         metadata_count: meta_count,
+        total_src_count: src_count,
     };
-
+    println!("-------------------------");
     println!("Parser Stats: {:?}", stats);
     Ok(())
 }
