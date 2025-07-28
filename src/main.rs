4be70254e9e0e7c41a32d2b4ab9d7ce029942937
shubham23471 <shubhamshakya61@gmail.com>
2025-07-28 12:45:15 +0530

    [DONE] added imagedata into struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[warn(unused_imports)]
 use warc::WarcReader;
-use warc::WarcHeader;
+// use warc::WarcHeader;
 use markup5ever_rcdom::{RcDom, Handle};
 use html5ever::parse_document;
 use html5ever::tendril::TendrilSink;
@@ -17,10 +17,18 @@ struct CountInfo {
     total_src_count: i32,
 }
 
+#[derive(Debug)]
+#[allow(dead_code)]
+struct ImageData {
+    record_id: String, 
+    src: String,
+    alt: String,
+}
+
 // &Handle: as we want to pass this without taking the ownership or cloning
 #[allow(unused_variables)]
-fn walk(node: &Handle, all_src: &mut Vec<String>, 
-        all_alt: &mut Vec<String>) {
+fn walk(node: &Handle, record_id: &str,
+    images: &mut Vec<ImageData>) {
 
     match &node.data {
         NodeData::Element {name, attrs, ..} => {
@@ -42,10 +50,12 @@ fn walk(node: &Handle, all_src: &mut Vec<String>,
                 if (src_value.starts_with("http:") || src_value.starts_with("https:")) 
                 && 
                 (src_value.ends_with("png") || src_value.ends_with("jpeg")) { 
-                    all_src.push(src_value); 
-                    if let Some(alt_value) = alt{
-                        all_alt.push(alt_value);
-                    }
+                    let image = ImageData {
+                        record_id: record_id.to_string(),
+                        src: src_value,
+                        alt: alt.unwrap_or("None".to_string())
+                    };
+                    images.push(image)
                 }
             }   
         },
@@ -53,7 +63,7 @@ fn walk(node: &Handle, all_src: &mut Vec<String>,
     }
     // recurse on children so we visit the whole tree
     for child in node.children.borrow().iter(){
-        walk(child, all_src, all_alt);
+        walk(child, record_id, images);
     }
 }
 
@@ -65,10 +75,9 @@ fn main() ->  Result<(), Box<dyn std::error::Error>> {
     let mut res_count = 0;
     let mut req_count = 0;
     let mut meta_count = 0;
-    let mut src_count: i32 = 0;
+    let  src_count: i32 = 0;
     
     for record in warc_file.iter_records(){
-        // println!("====");
         count += 1;
 
         // println!("{}: {}", WarcHeader::RecordID, record?.warc_id());
@@ -86,30 +95,26 @@ fn main() ->  Result<(), Box<dyn std::error::Error>> {
                 if record.warc_type().to_string() == "response"{
                     res_count += 1;
                     let response_body = String::from_utf8_lossy(&record.body());
-                    // println!("Body: {:?}", response_body);
+                    
 
                     let parser = parse_document(RcDom::default(), Default::default());
                     let dom =  parser.one::<String>(response_body.into());
-                    // same as calling:  parser.one(response_body.into()); just explicit about 
-                    // "I'm giving you a String and I expect it to convert into the right Tendril type."
                     
                     let document: Handle = dom.document;
-                    let mut all_src = Vec::new();
-                    let mut all_alt = Vec::new();
+                    let record_id = record.warc_id();
+                    let mut images = Vec::new();
 
-                    walk(&document, &mut all_src, &mut all_alt);
+                    walk(&document, &record_id, &mut images);
+                    // println!("here: {:?}", images);
+                    for img in images {
+                        println!("here: {:?}", img);
+                    }
 
-                    src_count += all_src.len() as i32;
-                    println!("{}: {}", WarcHeader::RecordID, record.warc_id());
-                    println!("SRC value: {}", all_src.len());
-                    println!("ALT value: {}", all_alt.len());
-                    println!("SRC value: {:?}", all_src);
-                    println!("ALT value: {:?}", all_alt);
-                    println!("-------------------------");
+                    // src_count += all_src.len() as i32;
+                    // println!("{}: {}", WarcHeader::RecordID, record.warc_id());
                     
 
                 } else if record.warc_type().to_string() == "request" {
-                    // println!("Body: {:?}", std::str::from_utf8((record.body()))?); 
                     req_count +=1;
                 } else if record.warc_type().to_string() == "metadata" {
                     meta_count +=1; 
